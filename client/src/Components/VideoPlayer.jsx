f31c4572fa5298951266674ad41691091b05043d
Feku Sharma <fekusharma111@gmail.com>
2022-08-25 16:16:21 +0530

    .mpd

--- client/src/Components/VideoPlayer.jsx
+++ client/src/Components/VideoPlayer.jsx
@@ -13,25 +13,35 @@ justify-content: center
 
 const VideoPlayer = () => {
   const [videoFilePath, setVideoFilePath] = useState(null);
-
+ 
   const handleVideoUpload = (e) => {
     setVideoFilePath(URL.createObjectURL(e.target.files[0]));
     console.log(e.target.files[0]);
+    
+  };
+
+  const config = {
+    attributes: {
+      disablePictureInPicture: true,
+      controlsList: 'nodownload'
+    }
   };
   return (
       <Container>
       <input type="file"
         onChange={handleVideoUpload}
       />
+
       <ReactPlayer 
-        url={videoFilePath}
-      
+              url={videoFilePath}
               controls
               // playing= {true}
-              config={{ file: { attributes: { controlsList: 'nodownload' } } }}
-              onContextMenu={e => e.preventDefault()}
- />
-      {/* <ReactPlayer url={videoFilePath} width="100%" height="100%" controls={true} /> */}
+               config={config
+        //   { file: { attributes: { controlsList: 'nodownload' } } }}
+        // onContextMenu={e => e.preventDefault()
+        }
+      >
+ </ReactPlayer>
  </Container>
   )
 }
